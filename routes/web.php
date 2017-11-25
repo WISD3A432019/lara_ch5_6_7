fee34a2b622a483b341de071bb5be4fcd65ccd7c
jkluio132465 <jkluio132465@gmail.com>
2017-11-26 01:42:22 +0800

    路由命名

--- routes/web.php
+++ routes/web.php
@@ -73,4 +73,17 @@ Route::group(['prefix' => 'student'], function($student_no) {
     })->where(['subject'=>'(chinese|english|math|)']);
 });
 
-   
+/*路由命名 */
+
+Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix' => 'student'],function($student_no) {
+    //
+    Route::get('/{student_no}',['as'=>'student','uses'=> function ($student_no){
+    return "學號:".$student_no;
+    }
+    ]);
+
+    Route::get('{student_no}/score/{subject?}',['as'=>'student.score','uses'=> function ($student_no,$subject=null) {
+        return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+    }])->where(['subject'=>'(chinese|english|math|)']);
+});
