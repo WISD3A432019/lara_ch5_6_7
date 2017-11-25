d62bac73a1ecf15336268c8e5fc9df8ed464d8ed
jkluio132465 <jkluio132465@gmail.com>
2017-11-26 00:51:43 +0800

    正規表達式限制參數

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::get('/', function () {
 });
 
 /*基礎路由*/
+
 Route::get('/student/{student_no}', function ($student_no) {
     return "學號:".$student_no;
 });
@@ -26,8 +27,33 @@ Route::get('/student/{student_no}/score', function ($student_no) {
 Route::get('/student/{student_no}/score/{subject}', function ($student_no,$subject) {
     return "學號:".$student_no."的".$subject."成績";
 });
+
 /*選擇性路由*/
 Route::get('/student/{student_no}/score/{subject?}', function ($student_no,$subject=null) {
     return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
 });
 
+
+/*正規表達式限制參數*/
+Route::group(['prefix' => 'lim'], function($student_no) {
+    //
+    Route::get('/student/{student_no}', function ($student_no) {
+        return "學號:".$student_no;
+    })->where(['student_no'=>'s[0-9]{10}']);
+
+    Route::get('/student/{student_no}/score/{subject?}', function ($student_no,$subject=null) {
+        return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+    })->where(['student_no'=>'s[0-9]{10}','subject'=>'(chinese|english|math|)']);
+
+   
+    Route::pattern('student_no','s[0-9]{10}');
+
+    Route::get('/student/{student_no}', function ($student_no) {
+    return "學號:".$student_no;
+    });
+
+    Route::get('/student/{student_no}/score/{subject?}', function ($student_no,$subject=null) {
+        return "學號:".$student_no."的".((is_null($subject))?"所有科目":$subject)."成績";
+    })->where(['subject'=>'(chinese|english|math|)']);
+
+});
