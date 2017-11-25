cc4f59bee9cd374ea1ca918fb0affd64b34a628f
jkluio132465 <jkluio132465@gmail.com>
2017-11-25 22:30:15 +0800

    ch5_路由設定

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/student/{student_no}', function ($student_no) {
+    return "學號:".$student_no;
+});
+Route::get('/student/{student_no}/score', function ($student_no) {
+    return "學號:".$student_no."所有成績";
+});
